0a1e17e14a841091b210b8c1d80a0802ad5fa5d7
et4321 <54795466+et4321@users.noreply.github.com>
2020-01-07 11:21:18 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<html>
+
+<h1> Form </h1>
+
+
+<form action="" method= "get" class= "form">
+
+    <div class= "form">
+        <label for = "email">Please enter your Email:
+        </label>
+        <input type = "email" id="email" id= "email" required>
+    </div>
+
+    <div class="form">
+        <input type="submit" value="Send Form">
+    </div>
+    
+</form>
+
+</html>
